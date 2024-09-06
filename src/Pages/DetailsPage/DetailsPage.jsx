201b8b68c703231c937e931ee09b48fd663236c6
Md Fahim Hossain <fahimreshad3@gmail.com>
2024-09-07 03:21:16 +0600

    updated the project

--- src/Pages/DetailsPage/DetailsPage.jsx
+++ src/Pages/DetailsPage/DetailsPage.jsx
@@ -27,7 +27,7 @@ const DetailsPage = () => {
         bio,
         userEmail: user.email,
       });
-      if(response.data){
+      if (response.data) {
         Swal.fire({
           position: "top-end",
           icon: "success",
@@ -45,240 +45,242 @@ const DetailsPage = () => {
   };
 
   return (
-      <div
-        key={bio._id}
-        className="flex flex-col lg:flex-row gap-10 container mx-auto mt-4 md:mt-12"
-      >
-        <Helmet>
+    <div
+      key={bio._id}
+      className="flex flex-col lg:flex-row gap-10 container mx-auto mt-4 md:mt-12"
+    >
+      <Helmet>
         <title>Match Mingle || DetailsPage</title>
       </Helmet>
-        <div className="">
-          <img
-            className="max-h-screen w-full rounded-lg mt-4 md:mt-6 lg:mt-0"
-            src={bio.profileImage}
-            alt=""
-          />
+      <div className="">
+        <img
+          className="max-h-screen w-full rounded-lg mt-4 md:mt-6 lg:mt-0"
+          src={bio.profileImage}
+          alt=""
+        />
+      </div>
+      <div className="text-black border ">
+        <div className="flex justify-between items-center">
+          <h2 className="text-[#66451c] text-5xl font-poppins font-semibold">
+            {bio.name}
+          </h2>
+          <button
+            onClick={handleAddToFavorite}
+            className="rounded-xl hover:cursor-pointer"
+          >
+            <MdFavorite className="text-4xl text-[#66451c]" />
+          </button>
         </div>
-        <div className="text-black border ">
-          <div className="flex justify-between items-center">
-            <h2 className="text-[#66451c] text-5xl font-poppins font-semibold">
-              {bio.name}
-            </h2>
-            <button
-              onClick={handleAddToFavorite}
-              className="rounded-xl hover:cursor-pointer"
-            >
-              <MdFavorite className="text-4xl text-[#66451c]" />
-            </button>
+        <div className="flex gap-4">
+          <div className="text-center bg-slate-100 pt-4 px-3 rounded-lg h-36 w-28">
+            <FaCity className="text-3xl text-[#66451c] " />
+            <h6 className="uppercase text-[#66451c] text-md font-thin border">
+              City
+            </h6>
+            <h5 className="uppercase text-[#66451c]">{bio.presentDivision}</h5>
           </div>
-          <div className="flex gap-4">
-            <div className="text-center bg-slate-100 pt-4 px-3 rounded-lg h-36 w-28">
-              <FaCity className="text-3xl text-[#66451c] " />
-              <h6 className="uppercase text-[#66451c] text-md font-thin border">
-                City
-              </h6>
-              <h5 className="uppercase text-[#66451c]">
-                {bio.presentDivision}
-              </h5>
-            </div>
-            <div className="text-center bg-slate-100 pt-4 px-3 rounded-lg h-36 w-28">
-              <FaSnowman className="text-3xl text-[#66451c] " />
-              <h6 className="uppercase text-[#66451c] text-md font-thin border">
-                Age
-              </h6>
-              <h5 className="uppercase text-[#66451c]">{bio.age}</h5>
-            </div>
-            <div className="text-center bg-slate-100 pt-4 px-3 rounded-lg h-36 w-28">
-              <GiBodyHeight className="text-3xl text-[#66451c] " />
-              <h6 className="uppercase text-[#66451c] text-md font-thin border">
-                Height
-              </h6>
-              <h5 className="uppercase text-[#66451c]">{bio.height}</h5>
-            </div>
-            <div className="text-center bg-slate-100 pt-4 px-3 rounded-lg h-36 w-28">
-              <IoCodeWorkingSharp className="text-3xl text-[#66451c] " />
-              <h6 className="uppercase text-[#66451c] text-md font-thin border">
-                Job
-              </h6>
-              <h5 className="uppercase text-[#66451c]">{bio.occupation}</h5>
-            </div>
+          <div className="text-center bg-slate-100 pt-4 px-3 rounded-lg h-36 w-28">
+            <FaSnowman className="text-3xl text-[#66451c] " />
+            <h6 className="uppercase text-[#66451c] text-md font-thin border">
+              Age
+            </h6>
+            <h5 className="uppercase text-[#66451c]">{bio.age}</h5>
           </div>
+          <div className="text-center bg-slate-100 pt-4 px-3 rounded-lg h-36 w-28">
+            <GiBodyHeight className="text-3xl text-[#66451c] " />
+            <h6 className="uppercase text-[#66451c] text-md font-thin border">
+              Height
+            </h6>
+            <h5 className="uppercase text-[#66451c]">{bio.height}</h5>
+          </div>
+          <div className="text-center bg-slate-100 pt-4 px-3 rounded-lg h-36 w-28">
+            <IoCodeWorkingSharp className="text-3xl text-[#66451c] " />
+            <h6 className="uppercase text-[#66451c] text-md font-thin border">
+              Job
+            </h6>
+            <h5 className="uppercase text-[#66451c]">{bio.occupation}</h5>
+          </div>
+        </div>
 
-          <div>
-            <h2 className="uppercase text-3xl text-[#66451c] font-poppins font-semibold">
-              Contact info
-            </h2>
+        <div>
+          <h2 className="uppercase text-3xl text-[#66451c] font-poppins font-semibold">
+            Contact info
+          </h2>
 
+          {isPremium ? (
+            <>
+              {" "}
+              <div className="flex items-center gap-6">
+                <MdContactPhone className="text-[#66451c] text-2xl border rounded-lg border-solid border-[#66451c] p-2" />
+                <h4 className="text-[#66451c] text-2xl border-[#66451c]">
+                  Phone: <span className="text-xl">{bio.mobileNumber}</span>
+                </h4>
+              </div>
+              <div className="flex items-center gap-6 -my-8">
+                <MdOutlineMail className="text-[#66451c] text-2xl border rounded-lg border-solid border-[#66451c] p-2" />
+                <h4 className="text-[#66451c] text-2xl border-[#66451c]">
+                  Email: <span className="text-xl">{bio.email}</span>
+                </h4>
+              </div>
+            </>
+          ) : (
+            ""
+          )}
+
+          <div className="flex items-center gap-6 -my-8">
+            <IoLocationSharp className="text-[#66451c] text-2xl border rounded-lg border-solid border-[#66451c] p-2" />
+            <h4 className="text-[#66451c] text-2xl border-[#66451c]">
+              Present Address:{" "}
+              <span className="text-xl">{bio.presentDivision}</span>
+            </h4>
+          </div>
+          <div className="flex items-center gap-6">
+            <IoLocationSharp className="text-[#66451c] text-2xl border rounded-lg border-solid border-[#66451c] p-2" />
+            <h4 className="text-[#66451c] text-2xl border-[#66451c]">
+              Permanent Address:{" "}
+              <span className="text-xl">{bio.permanentDivision}</span>
+            </h4>
+          </div>
+        </div>
+
+        <div>
+          <h2 className="uppercase text-3xl text-[#66451c] font-poppins font-semibold">
+            Personal Information
+          </h2>
+          <div className="grid lg:grid-cols-2 gap-x-2">
+            <div className="flex items-center -mb-8 gap-2 text-[#66451c]">
+              <IoIosArrowForward className="text-black" />
+              <div>
+                <h3 className="">Name: </h3>
+              </div>
+              <div>
+                <h4>{bio.name}</h4>
+              </div>
+            </div>
+            <div className="flex items-center -mb-8 gap-2 text-[#66451c]">
+              <IoIosArrowForward className="text-black" />
+              <div>
+                <h3 className="">Fathers Name: </h3>
+              </div>
+              <div>
+                <h4>{bio.fathersName}</h4>
+              </div>
+            </div>
+            <div className="flex items-center gap-2 -mb-8 text-[#66451c]">
+              <IoIosArrowForward className="text-black" />
+              <div>
+                <h3 className="">Mothers Name: </h3>
+              </div>
+              <div>
+                <h4>{bio.mothersName}</h4>
+              </div>
+            </div>
+            <div className="flex items-center gap-2 -mb-8 text-[#66451c]">
+              <IoIosArrowForward className="text-black" />
+              <div>
+                <h3 className="">Age: </h3>
+              </div>
+              <div>
+                <h4>{bio.age}</h4>
+              </div>
+            </div>
+            <div className="flex items-center gap-2 -mb-8 text-[#66451c]">
+              <IoIosArrowForward className="text-black" />
+              <div>
+                <h3 className="">Date of birth: </h3>
+              </div>
+              <div>
+                <h4>{bio.dateOfBirth.slice(0, 10)}</h4>
+              </div>
+            </div>
+            <div className="flex items-center gap-2 -mb-8 text-[#66451c]">
+              <IoIosArrowForward className="text-black" />
+              <div>
+                <h3 className="">Height: </h3>
+              </div>
+              <div>
+                <h4>{bio.height}</h4>
+              </div>
+            </div>
+            <div className="flex items-center gap-2 -mb-8 text-[#66451c]">
+              <IoIosArrowForward className="text-black" />
+              <div>
+                <h3 className="">Weight: </h3>
+              </div>
+              <div>
+                <h4>{bio.weight}</h4>
+              </div>
+            </div>
+            <div className="flex items-center gap-2 -mb-8 text-[#66451c]">
+              <IoIosArrowForward className="text-black" />
+              <div>
+                <h3 className="">Occupation: </h3>
+              </div>
+              <div>
+                <h4>{bio.occupation}</h4>
+              </div>
+            </div>
+            <div className="flex items-center gap-2 -mb-8 text-[#66451c]">
+              <IoIosArrowForward className="text-black" />
+              <div>
+                <h3 className="">Race: </h3>
+              </div>
+              <div>
+                <h4>{bio.race}</h4>
+              </div>
+            </div>
+            <div className="flex items-center gap-2 -mb-8 text-[#66451c]">
+              <IoIosArrowForward className="text-black" />
+              <div>
+                <h3 className="">Expected Partner Age: </h3>
+              </div>
+              <div>
+                <h4>{bio.expectedPartnerAge}</h4>
+              </div>
+            </div>
+            <div className="flex items-center gap-2 -mb-8 text-[#66451c]">
+              <IoIosArrowForward className="text-black" />
+              <div>
+                <h3 className="">Expected Partner Height: </h3>
+              </div>
+              <div>
+                <h4>{bio.expectedPartnerHeight}</h4>
+              </div>
+            </div>
             {isPremium ? (
               <>
-                {" "}
-                <div className="flex items-center gap-6">
-                  <MdContactPhone className="text-[#66451c] text-2xl border rounded-lg border-solid border-[#66451c] p-2" />
-                  <h4 className="text-[#66451c] text-2xl border-[#66451c]">
-                    Phone: <span className="text-xl">{bio.mobileNumber}</span>
-                  </h4>
+                <div className="flex items-center gap-2 -mb-8 text-[#66451c]">
+                  <IoIosArrowForward className="text-black" />
+                  <div>
+                    <h3 className="">Contact Email: {bio.email}</h3>
+                  </div>
+                  <div>
+                    <h4>{bio.contactEmail}</h4>
+                  </div>
                 </div>
-                <div className="flex items-center gap-6 -my-8">
-                  <MdOutlineMail className="text-[#66451c] text-2xl border rounded-lg border-solid border-[#66451c] p-2" />
-                  <h4 className="text-[#66451c] text-2xl border-[#66451c]">
-                    Email: <span className="text-xl">{bio.email}</span>
-                  </h4>
+                <div className="flex items-center gap-2 -mb-8 text-[#66451c]">
+                  <IoIosArrowForward className="text-black" />
+                  <div>
+                    <h3 className="">Mobile Number: </h3>
+                  </div>
+                  <div>
+                    <h4>{bio.mobileNumber}</h4>
+                  </div>
                 </div>
               </>
             ) : (
-              ""
+              <div className="mt-8 ">
+                <Link to={`/checkout/${bio._id}`}>
+                  <button className="bg-[#66451c] text-white p-2 rounded-lg font-poppins hover:cursor-pointer">
+                    Request Contact Info
+                  </button>
+                </Link>
+              </div>
             )}
-
-            <div className="flex items-center gap-6 -my-8">
-              <IoLocationSharp className="text-[#66451c] text-2xl border rounded-lg border-solid border-[#66451c] p-2" />
-              <h4 className="text-[#66451c] text-2xl border-[#66451c]">
-               Present Address: <span className="text-xl">{bio.presentDivision}</span>
-              </h4>
-            </div>
-            <div className="flex items-center gap-6">
-              <IoLocationSharp className="text-[#66451c] text-2xl border rounded-lg border-solid border-[#66451c] p-2" />
-              <h4 className="text-[#66451c] text-2xl border-[#66451c]">
-               Permanent Address: <span className="text-xl">{bio.permanentDivision}</span>
-              </h4>
-            </div>
-          </div>
-
-          <div>
-            <h2 className="uppercase text-3xl text-[#66451c] font-poppins font-semibold">
-              Personal Information
-            </h2>
-            <div className="grid lg:grid-cols-2 gap-x-2">
-              <div className="flex items-center -mb-8 gap-2 text-[#66451c]">
-                <IoIosArrowForward className="text-black" />
-                <div>
-                  <h3 className="">Name: </h3>
-                </div>
-                <div>
-                  <h4>{bio.name}</h4>
-                </div>
-              </div>
-              <div className="flex items-center -mb-8 gap-2 text-[#66451c]">
-                <IoIosArrowForward className="text-black" />
-                <div>
-                  <h3 className="">Fathers Name: </h3>
-                </div>
-                <div>
-                  <h4>{bio.fathersName}</h4>
-                </div>
-              </div>
-              <div className="flex items-center gap-2 -mb-8 text-[#66451c]">
-                <IoIosArrowForward className="text-black" />
-                <div>
-                  <h3 className="">Mothers Name: </h3>
-                </div>
-                <div>
-                  <h4>{bio.mothersName}</h4>
-                </div>
-              </div>
-              <div className="flex items-center gap-2 -mb-8 text-[#66451c]">
-                <IoIosArrowForward className="text-black" />
-                <div>
-                  <h3 className="">Age: </h3>
-                </div>
-                <div>
-                  <h4>{bio.age}</h4>
-                </div>
-              </div>
-              <div className="flex items-center gap-2 -mb-8 text-[#66451c]">
-                <IoIosArrowForward className="text-black" />
-                <div>
-                  <h3 className="">Date of birth: </h3>
-                </div>
-                <div>
-                  <h4>{bio.dateOfBirth.slice(0, 10)}</h4>
-                </div>
-              </div>
-              <div className="flex items-center gap-2 -mb-8 text-[#66451c]">
-                <IoIosArrowForward className="text-black" />
-                <div>
-                  <h3 className="">Height: </h3>
-                </div>
-                <div>
-                  <h4>{bio.height}</h4>
-                </div>
-              </div>
-              <div className="flex items-center gap-2 -mb-8 text-[#66451c]">
-                <IoIosArrowForward className="text-black" />
-                <div>
-                  <h3 className="">Weight: </h3>
-                </div>
-                <div>
-                  <h4>{bio.weight}</h4>
-                </div>
-              </div>
-              <div className="flex items-center gap-2 -mb-8 text-[#66451c]">
-                <IoIosArrowForward className="text-black" />
-                <div>
-                  <h3 className="">Occupation: </h3>
-                </div>
-                <div>
-                  <h4>{bio.occupation}</h4>
-                </div>
-              </div>
-              <div className="flex items-center gap-2 -mb-8 text-[#66451c]">
-                <IoIosArrowForward className="text-black" />
-                <div>
-                  <h3 className="">Race: </h3>
-                </div>
-                <div>
-                  <h4>{bio.race}</h4>
-                </div>
-              </div>
-              <div className="flex items-center gap-2 -mb-8 text-[#66451c]">
-                <IoIosArrowForward className="text-black" />
-                <div>
-                  <h3 className="">Expected Partner Age: </h3>
-                </div>
-                <div>
-                  <h4>{bio.expectedPartnerAge}</h4>
-                </div>
-              </div>
-              <div className="flex items-center gap-2 -mb-8 text-[#66451c]">
-                <IoIosArrowForward className="text-black" />
-                <div>
-                  <h3 className="">Expected Partner Height: </h3>
-                </div>
-                <div>
-                  <h4>{bio.expectedPartnerHeight}</h4>
-                </div>
-              </div>
-              {isPremium ? (
-                <>
-                  <div className="flex items-center gap-2 -mb-8 text-[#66451c]">
-                    <IoIosArrowForward className="text-black" />
-                    <div>
-                      <h3 className="">Contact Email: {bio.email}</h3>
-                    </div>
-                    <div>
-                      <h4>{bio.contactEmail}</h4>
-                    </div>
-                  </div>
-                  <div className="flex items-center gap-2 -mb-8 text-[#66451c]">
-                    <IoIosArrowForward className="text-black" />
-                    <div>
-                      <h3 className="">Mobile Number: </h3>
-                    </div>
-                    <div>
-                      <h4>{bio.mobileNumber}</h4>
-                    </div>
-                  </div>
-                </>
-              ) : (
-                
-                <div className="mt-8 ">
-                  <Link to={`/checkout/${bio._id}`}><button className="bg-[#66451c] text-white p-2 rounded-lg font-poppins hover:cursor-pointer">Request Contact Info</button></Link>
-                </div>
-              )}
-            </div>
           </div>
         </div>
       </div>
-
+    </div>
   );
 };
 
